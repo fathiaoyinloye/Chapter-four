5f876ddd172c351a84338332d1163ec3ae55ea37
fathiaoyinloye <you@example.com>
2025-09-15 04:54:17 +0100

    Chapter Four Assignments

---
+++ Binary.java
@@ -0,0 +1,7 @@
+public class Binary{
+	public static void main(String[] args){
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter number that must be 0 and 1: ");
+		int number = scanner.nextInt();
